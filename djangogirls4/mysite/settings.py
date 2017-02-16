3ffa7ead44af37fc46f9e34a7f7d18155e727676
leethn <leethn@gmail.com>
2017-02-16 20:30:09 +0900

    Modified templates to display posts from database.

--- djangogirls4/mysite/settings.py
+++ djangogirls4/mysite/settings.py
@@ -14,8 +14,12 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -56,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            TEMPLATES_DIR
+            TEMPLATE_DIR
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -121,4 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
